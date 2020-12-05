4c49b29e667e0f344f49ad5264d5af0a8aef3b0d
Ryan Liu <dio.ryanliu@hotmail.com>
2020-12-04 16:17:05 -0800

    Refactor day4

--- day4/solution.py
+++ day4/solution.py
@@ -1,31 +1,69 @@
 import os
 import re
 
-def solution():
-    all_passports = []
-    with open(os.path.join(os.path.abspath(os.getcwd()), "input.txt")) as read_file:
-        current_passport = []
-        for line in read_file:
-            curr = line.strip()
+def part_one():
+    custom_passport = CustomPassport()
 
-            if not curr:
-                Passport.process_data(current_passport, all_passports)
-                current_passport.clear()
-            else:
-                current_passport.append(curr)
+    return custom_passport.count_part_one()
 
-        # Check at end if there's unprocessed stuff
-        if len(current_passport) != 0:
-            Passport.process_data(current_passport, all_passports)
+def part_two():
+    custom_passport = CustomPassport()
 
-    valid_passports = 0
-    for passport in all_passports:
-        if passport.is_valid():
-            valid_passports += 1
+    return custom_passport.count_part_two()
 
-    return valid_passports
+
+class CustomPassport:
+    def __init__(self):
+        self.parse_input()
+
+    def parse_input(self):
+        self.passports = []
+        with open(os.path.join(os.path.abspath(os.getcwd()), "input.txt")) as read_file:
+            current_passport = []
+            for line in read_file:
+                curr = line.strip()
+
+                if not curr:
+                    self.passports.append(Passport.process_data(current_passport))
+                    current_passport.clear()
+                else:
+                    current_passport.append(curr)
+
+            # Check at end if there's unprocessed stuff
+            if len(current_passport) != 0:
+                self.passports.append(Passport.process_data(current_passport))
+
+    def count_part_one(self):
+        valid_passports = 0
+        for passport in self.passports:
+            if passport.is_valid_part_one():
+                valid_passports += 1
+
+        return valid_passports
+
+    def count_part_two(self):
+        valid_passports = 0
+        for passport in self.passports:
+            if passport.is_valid_part_two():
+                valid_passports += 1
+
+        return valid_passports
+
 
 class Passport:
+    @staticmethod
+    def process_data(values):
+        passport_values = {}
+        input_values = []
+
+        for line in values:
+            first = line.split()
+            for item in first:
+                second = item.split(":")
+                passport_values[second[0]] = second[1]
+
+        return Passport(**passport_values)
+
     def __init__(self, byr=None, iyr=None, eyr=None, hgt=None, hcl=None, ecl=None, pid=None, cid=None):
         self.byr = byr
         self.iyr = iyr
@@ -36,8 +74,11 @@ class Passport:
         self.pid = pid
         self.cid = cid
 
-    def is_valid(self):
-        # First check for Nones
+    def is_valid_part_one(self):
+        return not any(i is None for i in (self.byr, self.iyr, self.eyr, self.hgt, self.hcl, self.ecl, self.pid))
+
+    def is_valid_part_two(self):
+                # First check for Nones
         check = not any(i is None for i in (self.byr, self.iyr, self.eyr, self.hgt, self.hcl, self.ecl, self.pid))
         if not check:
             return False
@@ -97,16 +138,3 @@ class Passport:
             return False
 
         return True
-
-    @staticmethod
-    def process_data(values, output_list):
-        passport_values = {}
-        input_values = []
-
-        for line in values:
-            first = line.split()
-            for item in first:
-                second = item.split(":")
-                passport_values[second[0]] = second[1]
-
-        output_list.append(Passport(**passport_values))
