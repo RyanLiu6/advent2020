0c5beb1a899b518b379ac86beac86faca3f0381c
Ryan Liu <dio.ryanliu@hotmail.com>
2020-12-09 00:11:27 -0800

    Adding day9 solutions

---
+++ day9/main.py
@@ -0,0 +1,11 @@
+from solution import part_one, part_two
+
+
+if __name__ == "__main__":
+    result = part_one()
+
+    print(result)
+
+    result = part_two()
+
+    print(result)
---
+++ day9/solution.py
@@ -0,0 +1,64 @@
+import os
+import enum
+
+def part_one():
+    custom_encoding = CustomEncoding()
+
+    return custom_encoding.process_part_one()
+
+def part_two():
+    custom_encoding = CustomEncoding()
+
+    return custom_encoding.process_part_two()
+
+
+class CustomEncoding:
+    def __init__(self):
+        self.preamble = 25
+
+        self.parse_input()
+
+    def parse_input(self):
+        self.nums = []
+        with open(os.path.join(os.path.abspath(os.getcwd()), "input.txt")) as read_file:
+            for line in read_file:
+                self.nums.append(int(line))
+
+    def process_part_one(self):
+        counter = 0
+        for i in range(self.preamble, len(self.nums)):
+            val = self.nums[i]
+            prev_25 = self.nums[counter:counter + self.preamble]
+            counter += 1
+
+            if not self.two_sum(val, prev_25):
+                return val
+
+    def process_part_two(self):
+        problematic_number = self.process_part_one()
+        start, end = self.sliding_window(problematic_number)
+
+        cont_arr = self.nums[start:end+1]
+
+        return min(cont_arr) + max(cont_arr)
+
+    def two_sum(self, target, arr):
+        for num in arr:
+            counterpart = target - num
+            if counterpart in arr:
+                return True
+
+        return False
+
+    def sliding_window(self, target):
+        start, end = 0, 0
+
+        while True:
+            total = sum(self.nums[start:end + 1])
+
+            if total == target:
+                return (start, end)
+            elif total < target:
+                end += 1
+            else:
+                start += 1
